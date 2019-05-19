3b3068745b30442969c49705d947a7974d56651a
Zhijun Sheng <zhijunsheng@gmail.com>
2019-05-19 06:30:06 -0400

    created class CChessBoard; printed out empty board

--- CChessKnight.java
+++ CChessKnight.java
@@ -1,6 +1,28 @@
 class CChessKnight {
   public static final void main(String[] args) {
-    System.out.println("Hello Knight");
+    CChessBoard brd = new CChessBoard();
+    System.out.println(brd);
   }
 }
 
+class CChessBoard {
+  final static int rows = 10;
+  final static int cols = 9;
+  @Override
+  public String toString() {
+    String brdStr = "";
+    brdStr += " ";
+    for (int i = 0; i < cols; i++) {
+      brdStr += " " + i;
+    }
+    brdStr += "\n";
+    for (int row = 0; row < rows; row++) {
+      brdStr += row + "";
+      for (int col = 0; col < cols; col++) {
+        brdStr += " .";
+      }
+      brdStr += "\n";
+    }
+    return brdStr;
+  }
+}
